c951bd82279ee77c2cecaacf7d49454488f47fac
Kiran <approximatewalker@gmail.com>
2021-09-10 13:52:50 +1000

    Rich List updates
    - Top % figure
    - Total Supply
    - Added richList.html to index.html

--- Splinterlands/richList/richList.js
+++ Splinterlands/richList/richList.js
@@ -1,6 +1,10 @@
 function data_collector() {
   let context = this;
   let rowData = {};
+  let supply = 0;
+  let numbAccounts = 0;
+
+  let formatter = new Intl.NumberFormat('en-US').format;
 
   let types_names = {
     DEC: "DEC",
@@ -41,22 +45,47 @@ function data_collector() {
   this.sortData = function (data) {
     console.log(`Sort Data....`);
     rowData = data.richlist;
+    supply = data.total_quantity;
+    numbAccounts = data.total_accounts;
     context.makeTable();
   }
 
   this.makeTable = function () {
-    let header = `<th>#</th><th>Player</th><th>${el('type').value} Balance</th>`;
+    let header = `<th>#</th><th>Player</th><th>${types_names[el('type').value]} Balance</th>`;
     let body = ``;
 
+    let top = {10: 0, 50: 0, 100: 0, 200: 0};
+    let topKeys = Object.keys(top);
+
     rowData.forEach((player, i) => {
-      body += `<tr><td>${i+1}</td><td>${player.player}</td><td> ${parseFloat(player.balance).toFixed(3)}</td></tr>`;
+      topKeys.forEach((category, index)=>{
+        if (i <= category) top[category] += player.balance;
+      })
+      body += `<tr><td>${i + 1}</td><td>${player.player}</td><td> ${formatter(parseFloat(player.balance).toFixed(3))} (${(player.balance/supply*100).toFixed(2)}%)</td></tr>`;
     })
 
     el("content").innerHTML =
-      `<table>
+      `Total ${types_names[el('type').value]} in circulation: ${formatter(supply)} in ${formatter(numbAccounts)} accounts.<br><br>
+Percentage Owned by:
+<ul>
+  <li>Top 10: ${formatter(top[10].toFixed(3))} ${types_names[el('type').value]} (${(top[10]/supply*100).toFixed(2)}%)</li>
+  <li>Top 50: ${formatter(top[50].toFixed(3))} ${types_names[el('type').value]} (${(top[50]/supply*100).toFixed(2)}%) </li>
+  <li>Top 100: ${formatter(top[100].toFixed(3))} ${types_names[el('type').value]} (${(top[100]/supply*100).toFixed(2)}%)</li>
+  <li>Top 200: ${formatter(top[200].toFixed(3))} ${types_names[el('type').value]} (${(top[200]/supply*100).toFixed(2)}%)</li>
+</ul>
+      <table>
         ${header}
          ${body}
       </table>`;
     el('generate').disabled = false;
+
+    el('disclaimer').innerHTML = `Disclaimer: Human error could result in mistakes in the representation of the
+  above data. Extracted from
+  https://game-api.splinterlands.com/players/richlist?token_type=${el('type').value}.
+  <br>
+  <br>
+  <a href="https://github.com/kiokizz">GitHub</a>
+  | Check out my blogs <a href="https://hive.blog/@kiokizz">@kiokizz</a>
+  & <a href="https://hive.blog/@splinterstats">@splinterstats</a>`
   }
 }
