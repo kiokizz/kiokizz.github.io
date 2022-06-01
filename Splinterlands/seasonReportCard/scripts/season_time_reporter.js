0a82e282f9f6858848e5a679045e99969cbd9bdc
Kiran <approximatewalker@gmail.com>
2022-06-01 22:10:27 +1000

    Transitioning adjustment to new season naming.

--- Splinterlands/seasonReportCard/scripts/season_time_reporter.js
+++ Splinterlands/seasonReportCard/scripts/season_time_reporter.js
@@ -1,11 +1,12 @@
 function get_season_string(settings) {
-  let season = settings.season["name"].substr(-2);
-  if (season === "74") season = 75;
+  let season_id = settings.season.id - 13;
 
   let next_season = Date.parse(settings.season["ends"]);
   let remaining_time = next_season - new Date().getTime();
 
-  return `Current Season: ${season}. Time to post for Season ${season - 1}:
+  let season_flavour_text = settings.season["name"];
+  if (season_id === 74 || season_id === 75) season_flavour_text = `Ranked Rewards Season 1 (${season_id})`
+  return `Current Season: ${season_flavour_text}. Time to post for Season ${season_id - 1}:
    ${Math.floor(remaining_time / (1000 * 60 * 60 * 24))} Days 
    ${Math.floor((remaining_time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))} Hrs 
    ${Math.floor((remaining_time % (1000 * 60 * 60)) / (1000 * 60))} Min 
