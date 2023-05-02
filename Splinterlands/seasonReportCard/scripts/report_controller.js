6167682def543b406b9f38e11170aff23522edc4
Kiran <approximatewalker@gmail.com>
2023-05-02 21:54:28 +1000

    fix error caused by not parsing date
    - mistake made in  'update season end times to use API set dates'

--- Splinterlands/seasonReportCard/scripts/report_controller.js
+++ Splinterlands/seasonReportCard/scripts/report_controller.js
@@ -123,11 +123,11 @@ function report_controller() {
     report_array.season.id = data.season.id;
     report_array.season.name = data.season.name;
     report_array.season.nameNum = data.season.id - 13;
-    report_array.season_start = (await axios.get(`https://api2.splinterlands.com/season?id=${report_array.season.id - 2}`)).data.ends
-    report_array.season_end = (await axios.get(`https://api2.splinterlands.com/season?id=${report_array.season.id - 1}`)).data.ends
+    report_array.season_start = Date.parse((await axios.get(`https://api2.splinterlands.com/season?id=${report_array.season.id - 2}`)).data.ends)
+    report_array.season_end = Date.parse((await axios.get(`https://api2.splinterlands.com/season?id=${report_array.season.id - 1}`)).data.ends)
 
-    console.log(report_array.season_start)
-    console.log(report_array.season_end)
+    console.log(`start`, report_array.season_start)
+    console.log(`end`, report_array.season_end)
 
     // report_array.season_start = Date.parse(report_array.season.season_end_times[report_array.season.id - 2]);
     // report_array.season_end = Date.parse(report_array.season.season_end_times[report_array.season.id - 1]);
