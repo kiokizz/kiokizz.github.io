20f59b720e99cdde6ab9fbf8ed9e25db7a860be0
Kiran <approximatewalker@gmail.com>
2025-01-09 21:37:15 +1100

    Season 149 update title

--- Splinterlands/battles/data_files/149.js
+++ Splinterlands/battles/data_files/149.js
@@ -1,4 +1,4 @@
-title_string = `Card Usage Statistics - Ranked Rewards Season xx (Season 1xx/1xx)`
+title_string = `Card Usage Statistics - Ranked Rewards Season 62 (Season 136/149)`
 battles_string = `<h3>Battles: 705921</h3>
 <table class="w3-table w3-striped w3-bordered" id="battles" style="max-width: 500px">
     <tr>
