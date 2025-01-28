2881d6a0fb085c1ea2b362a2d82011bc56dc1662
Kiran <approximatewalker@gmail.com>
2025-01-28 20:56:16 +1100

    Google Traffic Tracking

--- Splinterlands/scripts/navigation_loader.js
+++ Splinterlands/scripts/navigation_loader.js
@@ -17,8 +17,30 @@ let rewards_cards_experimental_colour = `<div class="toggle-container">
 </div>`;
 
 document.getElementById("nav_div").innerHTML =
-  Object.keys(pages).reduce((html, href) => {
-    let classes = "buttons w3-bar-item w3-button w3-text-white w3-border-right"
-      + (location.pathname.split("/").pop() === href ? `${localStorage.theme !== 'dark' ? " w3-theme-d3" : " darken"}` : "");
-    return html + `<a id="${href}" href="${href}" class="${classes}">${pages[href]}</a>`;
-  }, '<div class="w3-bar w3-border-top">') + `${(location.pathname.split("/").pop() === "rewardCards.html" ? rewards_cards_experimental_colour : "")}</div>`;
+    Object.keys(pages).reduce((html, href) => {
+      let classes = "buttons w3-bar-item w3-button w3-text-white w3-border-right"
+          + (location.pathname.split("/").pop() === href ? `${localStorage.theme !== 'dark' ? " w3-theme-d3" : " darken"}` : "");
+      return html + `<a id="${href}" href="${href}" class="${classes}">${pages[href]}</a>`;
+    }, '<div class="w3-bar w3-border-top">') + `${(location.pathname.split("/").pop() === "rewardCards.html" ? rewards_cards_experimental_colour : "")}</div>`;
+
+import("https://www.googletagmanager.com/gtag/js?id=G-DB2Z620NEG").then((mod2) => {
+  console.log(`Google Analytics`);
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag('js', new Date());
+
+  let domain = window.location.hostname
+  if (domain === "https://splinterstats.net/") {
+    gtag('config', 'G-DB2Z620NEG');
+    console.log(`SplinterStats accessed via ${domain} (G-DB2Z620NEG)`)
+  }
+  else {
+    gtag('config', 'G-0QWWFX50FM');
+    console.log(`SplinterStats accessed via ${domain} (G-0QWWFX50FM)`)
+  }
+});
+
