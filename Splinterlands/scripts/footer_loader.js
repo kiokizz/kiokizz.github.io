649ed53e9e8812a5f317cc3f8e6b96ebce35202b
Kiran <approximatewalker@gmail.com>
2025-03-05 19:42:32 +1100

    Replace SPS Dao validator list with link to witness post.

--- Splinterlands/scripts/footer_loader.js
+++ Splinterlands/scripts/footer_loader.js
@@ -2,6 +2,6 @@ document.getElementById("footer_div").innerHTML =
     `<div class="w3-footer w3-theme-l2 w3-padding-16" style="max-height: 100px">
         <a href="https://github.com/kiokizz">👨‍💻 GitHub</a>
         | <a href="https://hive.blog/@splinterstats">✍️ Blog</a>
-        | <a href="https://thespsdao.github.io/SPS-Validator/validator-nodes">🖥️ Validator</a>
+        | <a href="https://peakd.com/splinterlands/@splinterstats/splinterstats-sps-validator-announcement">🖥️ Validator</a>
         | Developed by <a href="https://hive.blog/@kiokizz">@kiokizz</a>
     </div>`
